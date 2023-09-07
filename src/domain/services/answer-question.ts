965ffaec88eecc96f0cd990009b83532f0aa064b
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-07 11:59:38 -0300

    feat(answer-question service): create a answer on execute

---
+++ src/domain/services/answer-question.ts
@@ -0,0 +1,15 @@
+import { Answer } from "../entities/answer";
+
+interface IAnswerQuestionService {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionService {
+  execute({ instructorId, questionId, content }: IAnswerQuestionService) {
+    const answer = new Answer(content);
+
+    return answer;
+  }
+}
