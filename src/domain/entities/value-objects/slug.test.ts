c396c22b54bd5780fbfd2d5f57cd94258dd70bd4
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-07 13:16:37 -0300

    test: slug

---
+++ src/domain/entities/value-objects/slug.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("create slug from text", () => {
+  const slug = Slug.createFromText(
+    "Adquira: Taça Bubblejuice Classy - vale 350c",
+  );
+
+  expect(slug.value).toEqual("adquira-taca-bubblejuice-classy-vale-350c");
+});
