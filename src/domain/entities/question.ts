dfd101b0946822b0014dd4f45664c6fa349bc566
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-07 15:40:01 -0300

    feat(question entity): add bestAnswerId, createdAt and updatedAt fields

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,10 +3,13 @@ import { Entity } from "../../core/entities/entity";
 import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface IQuestion {
+  authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
   title: string;
   content: string;
-  authorId: UniqueEntityId;
   slug?: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<IQuestion> {
