f635a5a2303e16f53eb9e2f02a41da6bb1c4a170
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-07 13:17:00 -0300

    feat(question entity): apply slug object-value usage

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,22 +1,26 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface IQuestion {
   title: string;
   content: string;
   authorId: string;
+  slug?: Slug;
   id?: string;
 }
 
 export class Question {
   public id: string;
   public title: string;
+  public slug: Slug;
   public content: string;
   public authorId: string;
 
-  constructor({ authorId, content, title, id }: IQuestion) {
+  constructor({ authorId, content, title, id, slug }: IQuestion) {
     this.title = title;
     this.content = content;
     this.authorId = authorId;
     this.id = id ?? randomUUID();
+    this.slug = slug ?? Slug.createFromText(title);
   }
 }
