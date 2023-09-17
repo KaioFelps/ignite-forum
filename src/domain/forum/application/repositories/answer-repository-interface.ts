a57594507d200ea3fc22a3a7b8f4132b223a823a
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-17 11:10:19 -0300

    feat: add findById and delete methods to answer-repository

--- src/domain/forum/application/repositories/answer-repository-interface.ts
+++ src/domain/forum/application/repositories/answer-repository-interface.ts
@@ -2,4 +2,6 @@ import { Answer } from "../../enterprise/entities/answer";
 
 export interface IAnswerRepository {
   create(answer: Answer): Promise<void>;
+  delete(answer: Answer): Promise<void>;
+  findById(id: string): Promise<Answer | null>;
 }
--- test/repositories/in-memory-answer-repository.ts
+++ test/repositories/in-memory-answer-repository.ts
@@ -7,4 +7,15 @@ export class InMemoryAnswerRepository implements IAnswerRepository {
   async create(answer: Answer): Promise<void> {
     this.items.push(answer);
   }
+
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id);
+    this.items.splice(itemIndex, 1);
+  }
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toString() === id);
+
+    return answer ?? null;
+  }
 }
