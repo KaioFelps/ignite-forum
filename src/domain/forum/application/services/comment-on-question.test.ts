94b08a297d3ea241c289af498ad94fb575a52581
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-17 21:09:10 -0300

    feat: comment on question service

---
+++ src/domain/forum/application/services/comment-on-question.test.ts
@@ -0,0 +1,35 @@
+import { InMemoryQuestionRepository } from "test/repositories/in-memory-question-repository";
+import { InMemoryQuestionCommentRepository } from "test/repositories/in-memory-question-comment.repository";
+import { CommentOnQuestionService } from "./comment-on-question";
+import { MakeQuestionFactory } from "test/factories/make-question";
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository;
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository;
+let sut: CommentOnQuestionService;
+
+describe("Comment on question service", () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository();
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository();
+    sut = new CommentOnQuestionService(
+      inMemoryQuestionRepository,
+      inMemoryQuestionCommentRepository,
+    );
+  });
+
+  test("if it's possible to comment on a question", async () => {
+    const question = MakeQuestionFactory.execute();
+
+    await inMemoryQuestionRepository.create(question);
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: "Comentário teste",
+    });
+
+    expect(inMemoryQuestionCommentRepository.items[0].content).toEqual(
+      "Comentário teste",
+    );
+  });
+});
---
+++ src/domain/forum/application/services/comment-on-question.ts
@@ -0,0 +1,44 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Question } from "../../enterprise/entities/question";
+import { IQuestionRepository } from "../repositories/question-repository-interface";
+import { QuestionComment } from "../../enterprise/entities/question-comment";
+import { IQuestionCommentRepository } from "../repositories/question-comment-repository";
+
+interface ICommentOnQuestionService {
+  authorId: string;
+  questionId: string;
+  content: string;
+}
+
+interface ICommentOnQuestionResponse {
+  questionComment: QuestionComment;
+}
+
+export class CommentOnQuestionService {
+  constructor(
+    private questionRepository: IQuestionRepository,
+    private questionCommentRepository: IQuestionCommentRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    content,
+    questionId,
+  }: ICommentOnQuestionService): Promise<ICommentOnQuestionResponse> {
+    const question = await this.questionRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityId(authorId),
+      questionId: new UniqueEntityId(questionId),
+      content,
+    });
+
+    await this.questionCommentRepository.create(questionComment);
+
+    return { questionComment };
+  }
+}
