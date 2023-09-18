8e2dc9e602d799d7c517cdd9802fa4aa68bf48c6
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-17 21:58:38 -0300

    feat: add find by id and delete methods to answer repositories

--- test/repositories/in-memory-answer-comment-repository.ts
+++ test/repositories/in-memory-answer-comment-repository.ts
@@ -9,4 +9,17 @@ export class InMemoryAnswerCommentRepository
   async create(answerComment: AnswerComment): Promise<void> {
     this.items.push(answerComment);
   }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    );
+    this.items.splice(itemIndex, 1);
+  }
+
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find((item) => item.id.toString() === id);
+
+    return answerComment ?? null;
+  }
 }
