38ba1beef9d6367c2bcd197a76fc79b5f7bf4106
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-17 21:39:48 -0300

    feat: delete and find by id methods

--- test/repositories/in-memory-question-comment-repository.ts
+++ test/repositories/in-memory-question-comment-repository.ts
@@ -9,4 +9,19 @@ export class InMemoryQuestionCommentRepository
   async create(questionComment: QuestionComment): Promise<void> {
     this.items.push(questionComment);
   }
+
+  async delete(questionComment: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    );
+    this.items.splice(itemIndex, 1);
+  }
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id,
+    );
+
+    return questionComment ?? null;
+  }
 }
