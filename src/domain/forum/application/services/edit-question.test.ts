7c3c8b63d254211c63d0c80ecf4dfd556b99d749
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-24 23:37:38 -0300

    feat: edit question with attachment watched lists

--- src/domain/forum/application/services/edit-question.test.ts
+++ src/domain/forum/application/services/edit-question.test.ts
@@ -3,14 +3,22 @@ import { EditQuestionService } from "./edit-question";
 import { MakeQuestionFactory } from "test/factories/make-question";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentRepository } from "test/repositories/in-memory-question-attachment-repository";
+import { MakeQuestionAttachmentFactory } from "test/factories/make-question-attachment";
 
-let inMemoryRepository: InMemoryQuestionRepository;
+let inMemoryQuestionRepository: InMemoryQuestionRepository;
+let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository;
 let sut: EditQuestionService;
 
 describe("Edit question service", () => {
   beforeEach(() => {
-    inMemoryRepository = new InMemoryQuestionRepository();
-    sut = new EditQuestionService(inMemoryRepository);
+    inMemoryQuestionRepository = new InMemoryQuestionRepository();
+    inMemoryQuestionAttachmentRepository =
+      new InMemoryQuestionAttachmentRepository();
+    sut = new EditQuestionService(
+      inMemoryQuestionRepository,
+      inMemoryQuestionAttachmentRepository,
+    );
   });
 
   test("if it's possible to edit a question", async () => {
@@ -19,19 +27,38 @@ describe("Edit question service", () => {
       new UniqueEntityId("question-1"),
     );
 
-    await inMemoryRepository.create(newQuestion);
+    await inMemoryQuestionRepository.create(newQuestion);
+
+    inMemoryQuestionAttachmentRepository.items.push(
+      MakeQuestionAttachmentFactory.execute({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("1"),
+      }),
+      MakeQuestionAttachmentFactory.execute({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("2"),
+      }),
+    );
 
     await sut.execute({
       authorId: newQuestion.authorId.toString(),
       questionId: newQuestion.id.toString(),
       title: "Pergunta teste",
       content: "Conteúdo teste",
+      attachmentIds: ["1", "3"],
     });
 
-    expect(inMemoryRepository.items[0]).toMatchObject({
+    expect(inMemoryQuestionRepository.items[0]).toMatchObject({
       title: "Pergunta teste",
       content: "Conteúdo teste",
     });
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.getItems(),
+    ).toHaveLength(2);
+    expect(inMemoryQuestionRepository.items[0].attachments.getItems()).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("3") }),
+    ]);
   });
 
   test("if it's impossible to edit a question that doesn't belong to the user", async () => {
@@ -40,13 +67,14 @@ describe("Edit question service", () => {
       new UniqueEntityId("question-1"),
     );
 
-    await inMemoryRepository.create(newQuestion);
+    await inMemoryQuestionRepository.create(newQuestion);
 
     const response = await sut.execute({
       authorId: "wrong-author-id",
       questionId: newQuestion.id.toString(),
       title: "Pergunta teste",
       content: "Conteúdo teste",
+      attachmentIds: [],
     });
 
     expect(response.isLeft()).toBe(true);
--- src/domain/forum/application/services/edit-question.ts
+++ src/domain/forum/application/services/edit-question.ts
@@ -3,12 +3,17 @@ import { Question } from "../../enterprise/entities/question";
 import { IQuestionRepository } from "../repositories/question-repository-interface";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { IQuestionAttachmentRepository } from "../repositories/question-attachment-repository";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 interface IEditQuestionService {
   authorId: string;
   questionId: string;
   title: string;
   content: string;
+  attachmentIds: string[];
 }
 
 type IEditQuestionResponse = Either<
@@ -17,13 +22,17 @@ type IEditQuestionResponse = Either<
 >;
 
 export class EditQuestionService {
-  constructor(private questionRepository: IQuestionRepository) {}
+  constructor(
+    private questionRepository: IQuestionRepository,
+    private questionAttachmentRepository: IQuestionAttachmentRepository,
+  ) {}
 
   async execute({
     authorId,
     questionId,
     content,
     title,
+    attachmentIds,
   }: IEditQuestionService): Promise<IEditQuestionResponse> {
     const question = await this.questionRepository.findById(questionId);
 
@@ -35,8 +44,25 @@ export class EditQuestionService {
       return left(new NotAllowedError());
     }
 
+    const currentQuestionAttachments =
+      await this.questionAttachmentRepository.findManyByQuestionId(questionId);
+
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments,
+    );
+
+    const newQuestionAttachments = attachmentIds.map((attachmentId) =>
+      QuestionAttachment.create({
+        attachmentId: new UniqueEntityId(attachmentId),
+        questionId: question.id,
+      }),
+    );
+
+    questionAttachmentList.update(newQuestionAttachments);
+
     question.title = title;
     question.content = content;
+    question.attachments = questionAttachmentList;
 
     await this.questionRepository.save(question);
 
