008556063f1e143ab8923638ab538fdc517af700
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-09-24 23:26:38 -0300

    feat: question attachment repositories

---
+++ test/repositories/in-memory-question-attachment-repository.ts
@@ -0,0 +1,18 @@
+import { IQuestionAttachmentRepository } from "@/domain/forum/application/repositories/question-attachment-repository";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
+
+export class InMemoryQuestionAttachmentRepository
+  implements IQuestionAttachmentRepository
+{
+  public items: QuestionAttachment[] = [];
+
+  async findManyByQuestionId(
+    questionId: string,
+  ): Promise<QuestionAttachment[]> {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    );
+
+    return questionAttachments;
+  }
+}
